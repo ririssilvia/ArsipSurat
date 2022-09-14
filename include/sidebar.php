c0d7e0a928c768416e294ccf1ab9b976763bdb77
ririssilvia <64761595+ririssilvia@users.noreply.github.com>
2022-09-14 11:39:31 +0700

    first commit-arsipSurat

---
+++ include/sidebar.php
@@ -0,0 +1,33 @@
+<div class="sidebar sidebar-style-2">			
+			<div class="sidebar-wrapper scrollbar scrollbar-inner">
+				<div class="sidebar-content">
+					<ul class="nav nav-primary">
+						<li class="nav-section">
+							<span class="sidebar-mini-icon">
+								<i class="fa fa-ellipsis-h"></i>
+							</span>
+							<h4 class="text-section">Arsip Surat</h4>
+						</li>
+						<li class="nav-item active">
+						    <a href="index.php">
+								<i class="fas fa-layer-group"></i>
+								<p>Arsip</p>
+							</a>
+						</li>
+						<li class="nav-item">
+						<a href="about.php">
+								<i class="fas fa-th-list"></i>
+								<p>About</p>
+							</a>
+						</li>
+						<!-- <li class="nav-item">
+						   <a href="tabel.php">
+								<i class="fas fa-th-list"></i>
+								<p>tabel</p>
+						   </a>
+						   </a>
+						</li> -->
+					</ul>
+				</div>
+			</div>
+		</div>

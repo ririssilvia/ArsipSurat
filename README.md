215384cf3006bbee1b70cc62d08334d86e395ac7
ririssilvia <64761595+ririssilvia@users.noreply.github.com>
2022-09-16 09:10:30 +0700

    all done fix

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ArsipSurat
 Aplikasi arsip surat berbasis website yang digunakan untuk sertifikasi BNSP-DIKSI Polinema 2022
+
+# Hasil Project
+
+### Halaman Index Terdapat Fitur Search, unggah data arsip surat, hapus, download, lihat detail
+![plot](images/1.png)
+
+### Halaman unggah data arsip surat
+![plot](images/2.png)
+![plot](images/3.png)
+![plot](images/4.png)
+
+### Halaman download data arsip surat
+![plot](images/5.png)
+
+### Halaman download lihat detial arsip surat
+![plot](images/6.png)
+
+### Halaman edit arsip surat
+![plot](images/7.png)
+![plot](images/8.png)
+
+### Halaman delete arsip surat
+![plot](images/10.png)
+![plot](images/11.png)
+![plot](images/12.png)
+
+### Halaman cari data arsip surat
+![plot](images/14.png)
+
+### Halaman menu about
+![plot](images/13.png)
--- arsip.sql
+++ arsip.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Okt 2021 pada 05.40
--- Versi server: 10.4.14-MariaDB
--- Versi PHP: 7.2.33
+-- Generation Time: Sep 16, 2022 at 03:30 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `surat`
+-- Table structure for table `surat`
 --
 
 CREATE TABLE `surat` (
@@ -37,32 +37,33 @@ CREATE TABLE `surat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `surat`
+-- Dumping data for table `surat`
 --
 
 INSERT INTO `surat` (`id`, `nomor`, `kategori`, `judul`, `waktu`, `file`) VALUES
-(1, '2021/PD1/TU/001', 'Pengumuman', 'Nota Dinas WFH', '2021-10-25 13:14:00', 'pdfsurat/251020211314FILE1.pdf'),
-(2, '2021/PD2/TU/002', 'Undangan', 'Undangan Halal Bi Halal', '2021-10-26 05:11:00', 'pdfsurat/261020210511FILE2.pdf');
+(6, '2021/PD1/TU/001', 'Undangan', 'Rapat Umum Pemegang Saham Luar Biasa', '2022-09-16 02:51:00', 'pdfsurat/160920220251Surat_Resmi_Perusahaan-Undangan.pdf'),
+(7, '2021/PD1/TU/002', 'Undangan', 'Surat WFH', '2022-09-16 02:52:00', 'pdfsurat/160920220252Surat_Resmi_Perusahaan-Pemberitahuan.pdf'),
+(8, '2022/LSP/PMB', 'Pengumuman', 'Rapat Dinas Luar Kota Batam', '2022-09-16 03:07:00', 'pdfsurat/160920220307Surat_Resmi_Dinas-Pemberitahuan.pdf');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `surat`
+-- Indexes for table `surat`
 --
 ALTER TABLE `surat`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `surat`
+-- AUTO_INCREMENT for table `surat`
 --
 ALTER TABLE `surat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
